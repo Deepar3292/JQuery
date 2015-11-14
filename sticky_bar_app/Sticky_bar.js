1c4a20144901f8d0c812f5b2314f61abf1ef46d9
Deepar3292 <drdeepu31@gmail.com>
2015-11-15 01:38:14 +0530

    my first sticky bar app

--- sticky_bar_app/Sticky_bar.js
+++ sticky_bar_app/Sticky_bar.js
@@ -1,48 +1,39 @@
-/* The below code demonstrates a sticky bar functionality. Here the header and the footer sections are made sticky */
-
-// Any code written within below callback function will be executed when the HTML document is fully loaded
-function DocumentReady() {
-
-var stickyHeadTop = $(".head").offset().top;                                               //offset().top gets the current element's top coordinates
-//var bottom = $(window).height() - parseInt($(".head").height() + $(".body").height()); 
-//var stickyFootBot = $(".footer").offset().bottom;
-    
-var stickyHead = function() {
+ function load() {
+ 
+ var stickyHead = function() {
+ var stickyHeadTop = $(".head").offset().top;
 	var scrollTop = $(window).scrollTop();  // scrollTop() gets the co-ordinates of the scroll bar
-
-	if(scrollTop > stickyHeadTop) {         // if the scroll bar position is greater than header's co-ordinates meaning that the user has scrolled the page
-		$(".head").addClass("sticky");      // make the header sticky by adding the css class "sticky"
-	} else {
-		$(".head").removeClass("sticky");   // else remove the class
+console.log(stickyHeadTop);
+console.log(scrollTop);
+	if(stickyHeadTop <= scrollTop  ) {         // if the scroll bar position is greater than header's co-ordinates meaning that the user has scrolled the page
+		$(".head").addClass("sticky1");      // make the header sticky by adding the css class "sticky"
+	} else  {
+		$(".head").removeClass("sticky1");   // else remove the class
 	}
 };
-/*var stickyFoot = function() {
- var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
- if(scrollTop === 0) {
-  $('#scrollTop').replaceWith('style="position: fixed; right: 25px; bottom: 25px;"');
-}*/
+ var stickyfoot = function () {
+  	  
+      var top = $(".body").height();
+      var scrollTop = $(window).scrollTop();
+		console.log(top);
+		console.log(scrollTop);
+      if (scrollTop <= top) {
+      		
+         $(".footer").addClass("sticky");
+       
+      } else {
+      	
+          $(".footer").removeClass("sticky");
+          
+      }
+  };
+$(window).scroll(function () {
+	  stickyHead();
+	  stickyfoot();
+  });
 
-/*if(scrollBottom > stickyFootBot) {
-	$(".footer").addClass("stickyfoot").css("bottom","bottom");
-} else {
-	$(".footer").removeClass("stickyfoot");
+ 
 }
-}; */
-//stickyHead();
-//stickyFoot();
-$(window).scroll(function() {                 // when the user starts scrolling the callback stickyHead() is called i.e the JQuery event "scroll" gets triggered
-   
-	stickyHead(/*function() {
-				stickyFoot(); }*/);
-	
-});
-}
-
-$(document).ready(DocumentReady);  					// $ which is a shortcut for jQuery() class, instantiates the jQuery object and the ready() is method which will be assigned to ready event of JQuery which gets triggered when the html document is completely loaded
-
-/* Short method to assign handler to ready event of JQuery
-$(function() {
-      content here;
-});
-*/ 
-
+ 
+ 
+ $(document).ready(load);
